8e80e0f9573b37a5cd4778046b9177ffbb433b1b
HP <HP@vijidevesh2124.mshome.net>
2023-07-31 09:47:23 +0530

    Fourth commit

---
+++ src/com/cg/daytwo/Identifier.java
@@ -0,0 +1,12 @@
+package com.cg.daytwo;
+
+public class Identifier {
+	public static void main(String[] args) {
+		String name="Viji";
+		int age=19;
+		int id=36;
+		System.out.println(name+" "+ age +" "+ id+" ");
+
+	}
+
+}
