7f275b9cd301ce29d02f57d22dcd351ccf19ad31
HP <HP@vijidevesh2124.mshome.net>
2023-07-31 15:22:27 +0530

    Eleventh commit

--- src/com/cg/daythree/ForEach.java
+++ src/com/cg/daythree/ForEach.java
@@ -3,7 +3,7 @@ package com.cg.daythree;
 public class ForEach {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[] = {1,3,5,8,9};
+		int a[] = {1,2,5,8,9};
 		
 		for(int i:a) {
 			System.out.println(i);
